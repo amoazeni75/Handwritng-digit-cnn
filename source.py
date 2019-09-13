3a0d6a273338afc87c8233a0f501710f1cace500
Alireza <alisampo1375@gmail.com>
2019-09-13 10:58:35 +0430

    Update source.py
    a mcomplicated model replaced with previous one

--- source.py
+++ source.py
@@ -35,11 +35,14 @@ num_classes = y_test.shape[1]
 def baseline_model():
     # create model
     model = Sequential()
-    model.add(Convolution2D(32, 5, 5, input_shape=(1, 28, 28), activation='relu'))
+    model.add(Convolution2D(30, 5, 5, input_shape=(1, 28, 28), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(15, 3, 3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
+    model.add(Dense(50, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -49,7 +52,7 @@ def baseline_model():
 model = baseline_model()
 
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=10, batch_size=200,verbose=2)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200,verbose=2)
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
